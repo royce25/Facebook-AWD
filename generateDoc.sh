8b561b17c3b7d5b05cd321950d812874885729ea
Alexandre Hermann <hermann.alexandre@ahwebdev.fr>
2014-07-20 05:37:27 +0200

    Fix generate docs and ignore dirs

--- generateDoc.sh
+++ generateDoc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 rm -R ./docs
-phpdoc -d ./ -t ./docs/  --ignore "vendors/AHWEBDEV/Framework/src/AHWEBDEV/Framework/apc.php,vendors/Facebook/,vendors/autoload/,Resources/" --template="responsive" --title="Facebook AWD API"
+phpdoc -d ./ -t ./docs/  --ignore "vendor/facebook/,vendor/composer/,autoload.php,apc.php,Resources/" --template="responsive" --title="Facebook AWD API"
